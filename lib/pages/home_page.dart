0b8b70360b287a2625af41b58805d7317a81f679
menady0 <menady20@gmail.com>
2025-10-15 14:11:38 +0300

    Init Project

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: RichText(
+          text: const TextSpan(
+            style: TextStyle(
+              fontWeight: FontWeight.w500,
+              fontSize: 32,
+            ),
+            children: [
+              TextSpan(text: "News"),
+              TextSpan(
+                text: "App",
+                style: TextStyle(color: Colors.orange),
+              ),
+            ],
+          ),
+        ),
+      ),
+      body: const Text('Home Page'),
+    );
+  }
+}
